e816d4917da7e7a14c667c1540dce3f948dcc5bb
IlyaRaylyan <i0990755567@gmail.com>
2019-10-02 13:42:55 +0300

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,104 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <NavigationDrawer
+      :articles="articles"
+      :sources="sources"
+      v-on:get-ukr-news="fetchUkrNews()"
+      v-on:fetch-tech="fetchTechNews()"
+    />
+
+    <v-content>
+      <v-container fluid>
+        <v-row justify="center" align="center">
+          <v-col class="shrink">
+            <NewsSection v-for="article in articles" :article="article" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app color="blue-grey" class="white--text">
+      <span>newsapi.org</span>
+      <div class="flex-grow-1"></div>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import NavigationDrawer from "./components/NavigationDrawer.vue";
+import NewsSection from "./components/NewsSection.vue";
+import axios from "axios";
+export default {
+  components: { NewsSection, NavigationDrawer },
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null,
+    drawerRight: null,
+    right: false,
+    left: false,
+    articles: [],
+    sources: []
+  }),
+  created() {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=fd403a80d07e481ca95b323e3fbe49af"
+      )
+      .then(response => {
+        this.articles = response.data.articles;
+      })
+      .catch(e => {
+        console.log(e);
+      });
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    axios
+      .get(
+        "https://newsapi.org/v2/sources?apiKey=fd403a80d07e481ca95b323e3fbe49af"
+      )
+      .then(response => {
+        console.log(response);
+        this.sources = response.data.sources;
+        console.log("data:");
+        console.log(response.data.sources); // This will give you access to the full object
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  },
+  methods: {
+    fetchUkrNews() {
+      axios
+        .get(
+          "https://newsapi.org/v2/top-headlines?country=ua&apiKey=fd403a80d07e481ca95b323e3fbe49af"
+        )
+        .then(response => {
+          console.log(response);
+          this.articles = response.data.articles;
+          console.log("data:");
+          console.log(response.data); // This will give you access to the full object
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    fetchTechNews() {
+      axios
+        .get(
+          "https://newsapi.org/v2/top-headlines?country=ua&category=technology&apiKey=fd403a80d07e481ca95b323e3fbe49af"
+        )
+        .then(response => {
+          console.log(response);
+          this.articles = response.data.articles;
+          console.log("data:");
+          console.log(response.data); // This will give you access to the full object
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  }
+};
+</script>
